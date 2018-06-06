75287cc5b87b9fea034c9b6ed121e43079c29393
sdass <sdass@drf.com>
2018-06-06 16:06:54 -0400

    displayed 2 props

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("This is a test");
+
+/* This needs to be orchestrated with import or require
+var MyComponent = React.createClass({
+    render: function(){
+      return(
+        <h3>Create cmp!</h3>
+      );
+    }
+
+});
+
+*/
